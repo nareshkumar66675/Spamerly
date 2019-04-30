c8cde99a7069243a0a3ac2869f19bb99f7330f8c
Naresh Kumar <nareshkumar66675@gmail.com>
2019-04-30 18:42:06 -0500

    Added Data Acquire

---
+++ DataAcquire/MailScrap/MailScrap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MailScrap
+{
+    class MailScrap
+    {
+        private TcpClient _imapClient;
+        private NetworkStream _imapNs;
+        private StreamWriter _imapSw;
+        private StreamReader _imapSr;
+
+        public void InitializeConnection(string hostname, int port)
+        {
+            try
+            {
+                _imapClient = new TcpClient(hostname, port);
+                _imapNs = _imapClient.GetStream();
+                _imapSw = new StreamWriter(_imapNs);
+                _imapSr = new StreamReader(_imapNs);
+
+                Console.WriteLine("*** Connected ***");
+                  Response();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //return ex.Message;
+            }
+        }
+        public string Response()
+        {
+            byte[] data = new byte[_imapClient.ReceiveBufferSize];
+            int ret = _imapNs.Read(data, 0, data.Length);
+            return Encoding.ASCII.GetString(data).TrimEnd();
+        }
+
+        public string AuthenticateUser(string username, string password)
+        {
+            _imapSw.WriteLine("$ LOGIN " + username + " " + password);
+            _imapSw.Flush();
+            return Response();
+        }
+    }
+}
---
+++ DataAcquire/MailScrap/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AE.Net.Mail;
+
+namespace MailScrap
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //MailScrap mailScrap = new MailScrap();
+
+            //mailScrap.InitializeConnection("imap.gmail.com", 993);
+
+            //mailScrap.AuthenticateUser("nareshkumar66675@gmail.com", "naresh14GMAIL");
+
+            Console.WriteLine("Enter your Gmail Address:");
+            var addr = Console.ReadLine();
+
+            Console.WriteLine("Enter your Gmail Password(Trust me it is safe):");
+            var pwd = Console.ReadLine();
+
+            Console.WriteLine("Folder Path");
+            var path = Console.ReadLine();
+
+            using (var imap = new AE.Net.Mail.ImapClient("imap.gmail.com", addr, pwd, AuthMethods.Login, 993,true))
+            {
+                //var mb = imap.ListMailboxes("", "*");
+                imap.SelectMailbox("[Gmail]/Spam");
+
+
+                var count = imap.GetMessageCount();
+
+                Console.WriteLine($"Spam Count: {count}");
+
+                var msgs = imap.GetMessages(0, count - 1, false);
+                int i = 0;
+                foreach (var msg in msgs)
+                {
+                    File.WriteAllText($@"{path}\Spam{i}.txt", msg.Body);
+                    i++;
+                }
+
+                //var msgs = imap.SearchMessages(
+                //  SearchCondition.Undeleted().And(
+                //    SearchCondition.From("david"),
+                //    SearchCondition.SentSince(new DateTime(2000, 1, 1))
+                //  ).Or(SearchCondition.To("andy"))
+                //);
+
+            //Flags.
+            }
+            Console.WriteLine("You have been Hacked ;P");
+            Console.WriteLine("Press any key.");
+            Console.ReadLine();
+        }
+    }
+}
