1a2eff7cf6969d383f0318db93e85826720f76d5
Sandip Dey <sandipdey91@gmail.com>
2019-05-03 19:23:36 -0500

    Changed the code to consolidate with a single method

--- Models/SVM/svm_predict.py
+++ Models/SVM/svm_predict.py
@@ -13,20 +13,18 @@ np.random.seed(6)
 import preprocess_train as pre
 import svm_cls as svm
 import csv
-def main():
+# def main():
+def svm_model(text):
     word_occurance_dict={}
-    word_attributes_list=[]
     l=[]
     C=None
     show_progress = False
     Y_predict=[]
     pred_col=[]
     weight_opt_list=np.loadtxt(open("weight_opt_list.csv", "rb"), delimiter=",")
-    b_list=np.loadtxt(open("b_list.csv", "rb"), delimiter=",") 
-    res2= pd.read_csv('features.csv')
-    word_attributes_list=res2
-    #word_attributes_list=np.loadtxt(open("features.csv", "rb"), delimiter=",")
-    text=input("enter the input sentence")
+    b_list=np.loadtxt(open("b_list.csv", "rb"), delimiter=",")
+    word_attributes_list=np.loadtxt(open("features.csv", "rb"), delimiter=",")
+    # text=input("enter the input sentence")
     test_df=pre.preprocess_test_data(text)
     full_test_df= pre.set_test_word_attributes(test_df,word_attributes_list)
     #print(test_df)
@@ -36,7 +34,8 @@ def main():
     full_test_df['Y_predict'] =Y_predict1
     full_test_df['pred_col'] =pred_col1
     full_test_df['Y_prob']=Y_prob
-    print(full_test_df['Y_predict'])
-    print(full_test_df['Y_prob'])
-if __name__ == '__main__':
-    main()
+    # print(full_test_df['Y_predict'])
+    # print(full_test_df['Y_prob'])
+    return full_test_df['Y_prob']
+# if __name__ == '__main__':
+#     main()
